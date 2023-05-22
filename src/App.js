47cebb311b75e2fb2ef060a0a3868948a88e44c3
Brooklyn Longhurst <brooklyn.longhurst@gmail.com>
2023-05-22 16:10:54 -0600

    inital commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+/* TODO: implement code in this file */
+import "./App.css";
+
+function App() {
+  return (
+  <div>
+    <h1>Favorite Pokemon</h1>
+    <button>Add</button>
+    <ul>
+      <li>Pokemon</li>
+    </ul>
+  </div>
+  )
+}
+
+export default App;
